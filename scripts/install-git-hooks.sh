f52c8d505378d4cba11308ac0a56dd0341fab27f
Fabrice Nduwayo <fabrice.nduwayo12@gmail.com>
2026-07-08 18:27:28 -0500

    terminus: set repo git author to Fabrice Nduwayo in install-git-hooks.

--- scripts/install-git-hooks.sh
+++ scripts/install-git-hooks.sh
@@ -7,6 +7,9 @@ HOOKS="$REPO_ROOT/.githooks"
 
 chmod +x "$HOOKS/prepare-commit-msg"
 git -C "$REPO_ROOT" config core.hooksPath .githooks
+git -C "$REPO_ROOT" config user.name "Fabrice Nduwayo"
+git -C "$REPO_ROOT" config user.email "fabrice.nduwayo12@gmail.com"
 
 echo "Installed git hooks: core.hooksPath=.githooks"
+echo "Repo author: Fabrice Nduwayo <fabrice.nduwayo12@gmail.com>"
 echo "Co-authored-by: Cursor lines will be removed from local commits."
